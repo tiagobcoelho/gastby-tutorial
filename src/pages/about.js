d9977e2f488d45aab760df5a2e7feed531ac4b52
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2021-06-06 13:47:19 +0100

    project

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis natus ullam obcaecati illo magni alias quaerat esse vitae commodi, minima nostrum nihil ad minus hic in facere! Ab, adipisci ducimus. Quis ad beatae tempore harum iusto soluta minima nisi voluptate vero, nemo repellat voluptatibus mollitia ea, deleniti corporis sit unde.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis natus ullam obcaecati illo magni alias quaerat esse vitae commodi, minima nostrum nihil ad minus hic in facere! Ab, adipisci ducimus. Quis ad beatae tempore harum iusto soluta minima nisi voluptate vero, nemo repellat voluptatibus mollitia ea, deleniti corporis sit unde.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis natus ullam obcaecati illo magni alias quaerat esse vitae commodi, minima nostrum nihil ad minus hic in facere! Ab, adipisci ducimus. Quis ad beatae tempore harum iusto soluta minima nisi voluptate vero, nemo repellat voluptatibus mollitia ea, deleniti corporis sit unde.</p>
+      </div>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,41 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
+import '../styles/global.css'
+import * as styles from '../styles/home.module.css'
+import Img from 'gatsby-image'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+
+  const { fluid } = data.file.childImageSharp
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in   Manchester.</p>
+        <Link 
+          className={styles.btn}
+          to="/projects"
+        >
+          My Portfolio Projects
+        </Link>
+        </div>
+        <Img fluid={fluid}/>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: {eq: "banner.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
